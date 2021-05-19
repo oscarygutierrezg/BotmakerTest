1103eb0d4146d3096180497dade421286e2c9298
Oscar Gutierrez <ogutierrez@todo1.net>
2021-05-18 23:00:31 -0400

    Se agrega la integracion con joke-api

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import requests
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import traceback
+
+# Authenticate with slacker
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 SLACK_TOKEN = os.environ.get("SLACK_TOKEN_")
+JOKE_API_URL = os.environ.get("JOKE_API_URL_")
 
 
 
@@ -20,30 +24,49 @@ def amusebot(**payload):
     data = payload["data"]
     web_client = payload["web_client"]
     bot_id = data.get("bot_id", "")
-    # If a message is not send by the bot
-    if bot_id == "":
-        channel_id = data["channel"]
-
-        # Extracting message send by the user on the slack
-        text = data.get("text", "")
-        text = text.split(">")[-1].strip()
-
-
-        response = ""
-        if "help" in text.lower():
-            user = data.get("user", "")
-            response = f"Hi <@{user}>! I am AmuseBot :)"
-        else:
-            activity_json_response = requests.get("http://www.boredapi.com/api/activity/").json()
-            activity = activity_json_response['activity']
-            response = str(activity)
-        
-        # Sending message back to slack
-        web_client.chat_postMessage(channel=channel_id, text=response)
+    subtype = data.get("subtype", "")
+    try:
+        if bot_id == "" and subtype == "":
+            channels = web_client.conversations_list()['channels']
+            idS = []
+            for channel in channels:
+                idS.append(channel['id'])  
+            channel_id = data["channel"]
+            
+            text = ''
+            callBot = False
+            for block in data['blocks']:
+                for element in block["elements"]:
+                    for e in element["elements"]:
+                        if e['type']=='text':
+                            text = e['text']
+                        elif e['type']=='user':
+                            callBot = True
+            if not callBot and channel_id in idS:
+                return
+            params = list(filter(None, text.split(" ")))
+            if len(params) != 2:
+                response ='You must enter 2 parameters text (Search text) language (cs, de, en, es, fr, pt)'
+            else:
+                contains = params[0].strip()
+                lang = params[1].strip()
+            
+                joke_json_response = requests.get(JOKE_API_URL+"?lang="+lang+"&contains="+contains).json()
+    
+                if joke_json_response['error']:
+                    response = joke_json_response['message']
+                else:
+                    if joke_json_response['type']=='twopart':
+                        response = joke_json_response['setup']+'\n'+joke_json_response['delivery']
+                    else:
+                        response = joke_json_response['joke']
+            web_client.chat_postMessage(channel=channel_id, text=response,reply_broadcast=False)
+    except Exception:
+        traceback.print_exc()
 
 try:
     rtm_client = RTMClient(token=SLACK_TOKEN)
     print("Bot is up and running!")
     rtm_client.start()
-except Exception as err:
-    print(err)
+except Exception :
+    traceback.print_exc()
